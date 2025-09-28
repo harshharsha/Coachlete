5bf253c3470be41de3544d5ecb1f35537dc0036c
Hemanth <hemanthweb@aviac.in>
2025-09-28 21:11:51 +0530

    [feat] added blogs page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,6 +5,7 @@ import Info from "@/pages/Info/Info";
 import DotMenu from "@/pages/DotMenu/DotMenu";
 import NavBar from "@/pages/NavBar/NavBar";
 import { useState } from "react";
+import Blogs from "@/pages/Blogs/Blogs";
 
 export default function Home() {
   const [openDot, setOpenDot] = useState(false);
@@ -15,6 +16,7 @@ export default function Home() {
       <DotMenu setOpenDot={setOpenDot} openDot={openDot} />
       <Homesection />
       <Info />
+      <Blogs />
     </main>
   );
 }
